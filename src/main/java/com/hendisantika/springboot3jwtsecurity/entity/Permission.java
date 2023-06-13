9b9b367c80538d113b3264499181a02709071875
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:25:22 +0700

    #6 - Add Permission Enum ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/entity/Permission.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springboot3jwtsecurity.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/23
+ * Time: 06:24
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public enum Permission {
+
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    MANAGER_READ("management:read"),
+    MANAGER_UPDATE("management:update"),
+    MANAGER_CREATE("management:create"),
+    MANAGER_DELETE("management:delete");
+
+    @Getter
+    private final String permission;
+}
