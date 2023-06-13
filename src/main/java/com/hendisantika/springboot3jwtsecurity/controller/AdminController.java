a7f99af5e908128faee56efb8c8c63daa767f9a3
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:52:31 +0700

    #48 - Add get in AdminController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3jwtsecurity/controller/AdminController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springboot3jwtsecurity.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+    @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')")
+    public String get() {
+        return "GET:: admin controller";
+    }
 }
