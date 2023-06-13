9b51a827fdd69fdd7867c58a1cf1e3c23a8d58da
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:19:46 +0700

    #1 - Initial commit ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/SpringBoot3JwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot3jwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JwtSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3JwtSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboot3jwtsecurity/SpringBoot3JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboot3jwtsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JwtSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
