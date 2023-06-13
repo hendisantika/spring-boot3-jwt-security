dcf617b0aa1af501e4273deca2db9affc8b17f1d
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:36:04 +0700

    #19 - Add extractClaim in JwtService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3jwtsecurity/service/JwtService.java
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/service/JwtService.java
@@ -1,7 +1,10 @@
 package com.hendisantika.springboot3jwtsecurity.service;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,5 +30,10 @@ public class JwtService {
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 }
