6dccf234469ca693e7c3ae9ce191062fe6951358
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:37:42 +0700

    #25 - Add isTokenExpired in JwtService ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3jwtsecurity/service/JwtService.java
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/service/JwtService.java
@@ -79,4 +79,9 @@ public class JwtService {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
 }
