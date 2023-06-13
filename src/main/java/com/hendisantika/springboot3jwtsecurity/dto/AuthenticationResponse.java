f4ca4e6e934229ba24ba674613f3dc9c6fe37e7f
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:47:41 +0700

    #35 - Add AuthenticationResponse DTO ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/dto/AuthenticationResponse.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springboot3jwtsecurity.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/23
+ * Time: 06:47
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
