78bb5b70656f22db9008b3c23db2e3b46c7ee299
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:52:45 +0700

    #49 - Add post in AdminController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3jwtsecurity/controller/AdminController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/controller/AdminController.java
@@ -1,7 +1,9 @@
 package com.hendisantika.springboot3jwtsecurity.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,12 @@ public class AdminController {
     @PreAuthorize("hasAuthority('admin:read')")
     public String get() {
         return "GET:: admin controller";
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
+    @Hidden
+    public String post() {
+        return "POST:: admin controller";
     }
 }
