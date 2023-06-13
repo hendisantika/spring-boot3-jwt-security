4a25d31cb7612b26bffabfffe829596f017ef492
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:53:59 +0700

    #53 - Add sayHello in DemoController ✏️📗:octocat:⬆

--- src/main/java/com/hendisantika/springboot3jwtsecurity/controller/DemoController.java
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/controller/DemoController.java
@@ -1,6 +1,8 @@
 package com.hendisantika.springboot3jwtsecurity.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo-controller")
 @Hidden
 public class DemoController {
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint");
+    }
 
 }
