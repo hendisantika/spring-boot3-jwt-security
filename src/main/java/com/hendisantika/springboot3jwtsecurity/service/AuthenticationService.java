7e6682aedfb3dd8c03ec9b7de54b3a51a65f19e9
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:48:43 +0700

    #37 - Add AuthenticationService ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/service/AuthenticationService.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springboot3jwtsecurity.service;
+
+import com.hendisantika.springboot3jwtsecurity.repository.TokenRepository;
+import com.hendisantika.springboot3jwtsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/23
+ * Time: 06:48
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+}
