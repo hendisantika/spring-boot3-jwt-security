d61b579db7f168163f1ab6b255892fb8a256ea2d
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:35:27 +0700

    #17 - Add JwtService ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/service/JwtService.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springboot3jwtsecurity.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/23
+ * Time: 06:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+}
