1df32736a80ea08275fe96a979e625aeb93aac1f
hendisantika <hendisantika@yahoo.co.id>
2023-06-14 06:53:36 +0700

    #52 - Add DemoController in AdminController ✏️📗:octocat:⬆

---
+++ src/main/java/com/hendisantika/springboot3jwtsecurity/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springboot3jwtsecurity.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot3-jwt-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/23
+ * Time: 06:53
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+@Hidden
+public class DemoController {
+
+}
